93608c56c612b61e0db4435fcf68d69ce680d960
Logan Hylton <lhyltoncs@gmail.com>
2022-05-31 13:19:35 -0400

    Website Creation and added day 1

---
+++ Lessons/Day1/Day1Assignment.java
@@ -0,0 +1,45 @@
+
+public class Day1Assignment {
+
+	/*
+	 * Problem 1: Create a function that returns the remainder you would get from dividing 
+	 * 	two numbers a and b, where a is divided by b.
+	 * 
+	 * Example: a = 4; b = 3; your function should return 1
+	 * Example: a = 4; b = 2; your function should return 0
+	 * 
+	 * For testing purposes your function must be named remainderAB
+	 */
+	
+	
+	
+	/*
+	 * Problem 2: Create a function that returns the product of 3 numbers, a, b, and c.
+	 * 
+	 * Example: a = 4; b = 3; c = 5; your function should return 60
+	 * Example: a = 4; b = 2; c = -1; your function should return -8
+	 * 
+	 * For testing purposes your function must be named productABC
+	 */
+	
+	
+	
+	/*
+	 * Problem 3 (Bonus: a little difficult): create a function that when given a 
+	 * 	decimal money value, a, it returns only the cents in decimal form.
+	 * 
+	 * Example: a = 4.01; your function should return 0.01
+	 * Example: a = 1.50; your function should return 0.50 or 0.5
+	 * 
+	 * For testing purposes your function must be named centsA
+	 */
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		// Your testing goes here
+		
+	}
+}
