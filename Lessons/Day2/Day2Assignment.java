5f4d80ae3b56199c1f0ed56e2e5f829686e37032
Logan Hylton <lhyltoncs@gmail.com>
2022-05-31 21:35:12 -0400

    add gitignore

---
+++ Lessons/Day2/Day2Assignment.java
@@ -0,0 +1,39 @@
+
+public class Day2Assignment {
+
+	/*
+	 * Problem 1: Create a function that takes in an integer 
+	 * 			  and returns the factorial of that number.
+	 * 
+	 * Example Test Case: factorial(5) => 5 * 4 * 3 * 2 * 1 => 120
+	 * 
+	 * Your function must be named 'factorial'
+	 */
+	
+	
+	
+	/*
+	 * Problem 2: Create a function that will print the 
+	 * 			  first 5 multiples of an integer to the console.
+	 * 
+	 * Example Test Case: firstFiveMultiples(2) => should print 4, 6, 8, 10, 12
+	 * 
+	 * Your function must be named 'firstFiveMultiples'
+	 */
+	
+	
+	
+	/*
+	 * Bonus: Create a function that returns the largest prime 
+	 * 		  integer less than a given integer.
+	 * 
+	 * Example Test Case: largestPrimeBelow(20) => 19 
+	 * 
+	 * Your function must be named 'largestPrimeBelow'
+	 */
+	
+	
+	public static void main(String[] args) {
+		// Your testing goes here
+	}
+}
