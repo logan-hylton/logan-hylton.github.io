9679664a610d1dd8cf54365914a740dd95f92482
Logan Hylton <lhyltoncs@gmail.com>
2022-06-02 12:11:24 -0400

    Added Day 3

---
+++ Lessons/Day3/Day3Assignment.java
@@ -0,0 +1,39 @@
+
+public class Day3Assignment {
+
+	/*
+	 * Problem 1: Create a function that returns an returns
+	 * 			  the largest int in a given array of ints.
+	 * 
+	 * Example: findLargest([1, 2, 3, 4, 5]) should return 5
+	 * Example: findLargest([-20, -3, 45, 23, -5, 1, -57, 37, 9]) should return  45
+	 */
+	
+	
+	
+	/*
+	 * Problem 2: Create a function that takes in an array of
+	 * 			  ints and returns an array in reverse order.
+	 * 
+	 * Example: reverse([1, 3, 2, 5, 4]) should return [4, 5, 2, 3, 1]
+	 */
+	
+	
+	
+	/*
+	 * Problem 3: Create a function that takes in two arrays,
+	 * 			  'arrayOne' and 'arrayTwo' and returns the 
+	 * 			  sum of the elements in 'arrayOne' with the 
+	 * 			  indexes in 'arrayTwo'.
+	 * 
+	 * Example: sumIndexes([1,2,3,4,5], [2,3]) should calculate 3 + 4 and return 7
+	 */
+	
+	
+	
+	public static void main(String[] args) {
+		// Your testing goes here
+
+	}
+
+}
